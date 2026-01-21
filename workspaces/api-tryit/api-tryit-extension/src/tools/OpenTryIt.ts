f3f68f945dd8bc851838596566d361352081408f
SGeesan <sugithgeesan@gmail.com>
2026-01-21 12:06:19 +0530

    Refactor: OpenTryIt invocation

--- workspaces/api-tryit/api-tryit-extension/src/tools/OpenTryIt.ts
+++ workspaces/api-tryit/api-tryit-extension/src/tools/OpenTryIt.ts
@@ -2,15 +2,17 @@ import * as vscode from 'vscode';
 export default class OpenTryIt implements vscode.LanguageModelTool<void> {
     
     async invoke(_options: vscode.LanguageModelToolInvocationOptions<void>, _token: vscode.CancellationToken): Promise<vscode.LanguageModelToolResult | null | undefined> {
-        const choice = await vscode.window.showInformationMessage('Open with Try It?', { modal: true }, 'Open');
-        if (choice !== 'Open') {
-            return new vscode.LanguageModelToolResult([
-                new vscode.LanguageModelTextPart('User cancelled opening Try It panel.')
-            ]);
-        }
         vscode.commands.executeCommand('api-tryit.openTryIt');
         return new vscode.LanguageModelToolResult([
             new vscode.LanguageModelTextPart('API Try It panel opened.')
         ]);
+    }
+    prepareInvocation(options: vscode.LanguageModelToolInvocationPrepareOptions<void>, token: vscode.CancellationToken): vscode.ProviderResult<vscode.PreparedToolInvocation> {
+        return {
+            confirmationMessages: {
+                title: 'Open API Try It',
+                message: new vscode.MarkdownString('Open this file in Try It view')
+            }
+        };
     }
 }
